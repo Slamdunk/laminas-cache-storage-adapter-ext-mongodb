1e09de7bec486c31c64957bb09be7683775a5ecf
Filippo Tessarotto <zoeslam@gmail.com>
2021-05-04 10:11:43 +0200

    test1

--- src/ExtMongoDb.php
+++ src/ExtMongoDb.php
@@ -3,7 +3,7 @@
 /**
  * @see       https://github.com/laminas/laminas-cache for the canonical source repository
  * @copyright https://github.com/laminas/laminas-cache/blob/master/COPYRIGHT.md
- * @license   https://github.com/laminas/laminas-cache/blob/master/LICENSE.md New BSD License
+ * @license   https://github.com/laminas/laminas-cache/blob/master/LICENSE.md New BSD
  */
 
 namespace Laminas\Cache\Storage\Adapter;
